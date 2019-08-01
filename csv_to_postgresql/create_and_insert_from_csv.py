b498e5ee29f5031a04c3be6d1c9e840a6121764a
martynov <kickrop@gmail.com>
2019-08-01 18:39:08 +0300

    working version
    only 1 data type

--- csv_to_postgresql/create_and_insert_from_csv.py
+++ csv_to_postgresql/create_and_insert_from_csv.py
@@ -2,7 +2,6 @@ import psycopg2
 import csv
 from tqdm import tqdm
 
-#csv_data = csv.reader(file('s_okopf.csv'))
 
 conn = psycopg2.connect(host="172.18.207.27", dbname="cases", user="dmartynov", password="LKhjkljklkwJBJjkw%499lekc$68")
 cur = conn.cursor()
@@ -23,6 +22,7 @@ def create_table_with_csvheader():
         )
         """.format(table_name, header_to_insert))
         conn.commit()
+        print("{} created successfully".format(table_name))
 #cur.execute("select * from statregistr.s_okfs;")
 #for record in cur:
 #    print(record)
@@ -30,35 +30,4 @@ def insert():
     with open('s_okopf.csv', 'r') as f:
         cur.copy_expert('COPY statregistr.test_2 FROM STDIN WITH CSV HEADER', f)
     conn.commit()
-
-def create_table():
-    cur.execute("""
-    CREATE TABLE IF NOT EXISTS statregistr.{}(
-    okopf text NOT NULL,
-    name text NOT NULL
-    )
-    """.format(table_name))
-    conn.commit()
-    print("{} created successfully".format(table_name))
-def insert_into_table():
-    with open(table_name + '.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader)
-        for row in tqdm(reader):
-            #print(row)
-            cur.execute(
-            "INSERT INTO statregistr." + table_name + " VALUES (%s, %s)", 
-            row
-            #"INSERT INTO statregistr.{} VALUES ({})".format(table_name,row) 
-            )
-    conn.commit()
     print('Inserted successfully')
-#def insert_into_table():
-#    with open('s_okopf.csv', 'r') as f:
-#        next(f)
-#        cur.copy_from(f, 'statregistr.s_okopf', sep=';')
-#    conn.commit()
-#create_table()
-create_table_with_csvheader()
-#insert()
-#insert_into_table()
