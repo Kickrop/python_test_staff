3db01e5ec98351db4c6e978dd34d2dfae8e6a8ac
martynov <kickrop@gmail.com>
2019-08-09 16:36:29 +0300

    added settings file for storing sensetive data

--- csv_to_postgresql/create_and_insert_from_csv.py
+++ csv_to_postgresql/create_and_insert_from_csv.py
@@ -1,34 +1,34 @@
 import psycopg2
 import csv
+import settings
 from tqdm import tqdm
 
-
-conn = psycopg2.connect(host="172.18.207.27", dbname="cases", user="dmartynov", password="LKhjkljklkwJBJjkw%499lekc$68")
+conn = psycopg2.connect(host=settings.sr_host, dbname="cases", user=settings.sr_user, password=settings.sr_password)
 cur = conn.cursor()
-table_name = 'test_2'
+table_name = 'test_3'
+file_name = ''
 
 def create_table_with_csvheader():
-    with open('s_okopf.csv', 'r') as f:
+    with open(file_name, 'r') as f:
         csv_reader = csv.reader(f)
         csv_header = next(csv_reader)
         header_to_insert = ''
         for i in csv_header:      
             header_to_insert = header_to_insert+ (i + ' text NOT NULL,')
         header_to_insert = header_to_insert[0:-1]
-        #print(header_to_insert)
-        cur.execute("""
-        CREATE TABLE IF NOT EXISTS statregistr.{}(
-        {}
+        cur.execute(f"""
+        CREATE TABLE IF NOT EXISTS statregistr.{table_name}(
+        {header_to_insert}
         )
-        """.format(table_name, header_to_insert))
+        """)
         conn.commit()
-        print("{} created successfully".format(table_name))
+        print(f"{table_name} created successfully")
 #cur.execute("select * from statregistr.s_okfs;")
 #for record in cur:
 #    print(record)
 def insert_into_table():
-    with open('s_okopf.csv', 'r') as f:
-        cur.copy_expert('COPY statregistr.test_2 FROM STDIN WITH CSV HEADER', f)
+    with open(file_name, 'r') as f:
+        cur.copy_expert(f'COPY statregistr.{table_name} FROM STDIN WITH CSV HEADER', f)
     conn.commit()
     print('Inserted successfully')
 
